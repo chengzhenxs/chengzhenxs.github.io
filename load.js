aa3a53e0ee16125158ea9c7f38ef7c8c76c2cead
侯翊超 <1506894594@qq.com>
2016-12-02 07:37:28 +0800

    Improve loading function

--- load.js
+++ load.js
@@ -1,9 +1,9 @@
 function loadContent(name) {
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function() {
-		//if (request.readyState == 4 && request.status == 200) {
+		if (request.readyState == 4 && request.status == 200) {
 			document.getElementById("content").innerHTML = request.responseText;
-		//}
+		}
 	}
 	request.open("GET", name + ".html?t=" + Math.random(), true);
 	request.send();
