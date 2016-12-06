c7f626379c92ee92ff6d2fc1495366ec7553d29c
侯翊超 <1506894594@qq.com>
2016-12-06 09:38:08 +0800

    Fix a bug that blocks formulae get rendered

--- load.js
+++ load.js
@@ -2,8 +2,8 @@ function loadContent(name) {
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function() {
 		if (request.readyState == 4 && request.status == 200) {
-			MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 			document.getElementById("content").innerHTML = request.responseText;
+			MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 		}
 	}
 	request.open("GET", name + ".html?t=" + Math.random(), true);
